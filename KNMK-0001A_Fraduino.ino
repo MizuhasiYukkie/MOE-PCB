eeb34b1dfa37ee40ddc8e705aa4b22af696a024e
Mizuhasi Yukkie <101932667+MizuhasiYukkie@users.noreply.github.com>
2022-08-16 19:01:07 +0900

    ver 1.1
    fix

--- KNMK-0001A_Fraduino.ino
+++ KNMK-0001A_Fraduino.ino
@@ -1,5 +1,5 @@
 /*!
- * KNMK-0001A Fraduino向けソースコード 1.0
+ * KNMK-0001A Fraduino向けソースコード 1.1
  *
  * Copyright (c) 2022 Mizuhasi Yukkie
  * This software is released under the MIT license.
@@ -22,28 +22,30 @@
  * 
  * 既知の基板上のエラー：HWB端子をプルダウンするのを忘れてしまいました。放っておいても基本的にロー状態なので無害ですが、
  * もし書き込み後に裏面LEDが高速点滅して言うことを聞かない……などが起きた場合は10kΩでGndに落とすと安定するかもしれません。
+ * 
+ * 1.1：少し見やすく編集、機能の更新は無し。使用していないシリアル通信の初期化をコメントアウト
  */
 
 #include <ADCTouch.h>
 #include <Adafruit_NeoPixel.h>
 
-#define LED1 0
+#define LED0 13 // 通常の単色LED
+#define LED1 0  // 基板上の表示に対応
 #define LED2 1
 #define LED3 2
 #define LED4 3
 #define LED5 4
 #define LED6 5
 #define LED7 6
-#define RGBLED_NUM 7
-#define RGBLED_PIN 6 // NeoPixel pin
-#define BLINK_LED 13 // Normal LED pin
+#define RGBLED_NUM 7 // RGBLEDの数
+#define RGBLED_PIN 6 // NeoPixel接続ピン
 
-#define KAMI 0
-#define MUNE 1
+#define KAMI  0  //タッチ判別ID
+#define MUNE  1
 #define SKIRT 2
 
-#define KAMI_P A0 //タッチセンシングのポート設定
-#define MUNE_P A1
+#define KAMI_P  A0 //タッチセンシングのポート設定（基板により違うので注意）
+#define MUNE_P  A1
 #define SKIRT_P A2
 
 Adafruit_NeoPixel pixels(RGBLED_NUM, RGBLED_PIN, NEO_GRB + NEO_KHZ800);
@@ -51,19 +53,20 @@ Adafruit_NeoPixel pixels(RGBLED_NUM, RGBLED_PIN, NEO_GRB + NEO_KHZ800);
 uint8_t BRT_LEVEL=1;//明るさレベル0-3　起動時は1から開始
 uint8_t BRT_VAL[]={10,25,65,175};//段階ごとの明るさ詳細値　お好みで変更を。
 uint8_t BRT_RND[]={40,65,155,255};//ランダムで変更する値（きらきら効果）
-uint8_t V_raw[10];//各LEDごとの明るさ実際の値（自動追従）
 
+uint8_t V_raw[RGBLED_NUM];//各LEDごとの明るさ実際の値（自動追従）
 uint8_t FuryGuage=0;//0-255 お胸を触り続けると溜まる
 
-int   T_offset[3];//タッチセンシングの初期値
-bool  T_flag[3];//タッチセンシングフラグ
-int PATTERN_MODE = 0;//モード切り替え（必要なら）
+int   T_offset[3];  //タッチセンシングの初期値
+bool  T_flag[3];    //タッチセンシングフラグ
+
+uint8_t PATTERN_MODE = 0;//点灯パターン 0-2
 
 
 void setup() {
-  pinMode(BLINK_LED, OUTPUT);
-  digitalWrite(BLINK_LED, LOW);//ON
-  Serial.begin(19200);
+  pinMode(LED0, OUTPUT);
+  digitalWrite(LED0, LOW);//ON
+//  Serial.begin(19200);
   pixels.begin();
 
   //タッチセンシングの初期値を登録
@@ -72,19 +75,16 @@ void setup() {
   T_offset[SKIRT] = ADCTouch.read(SKIRT_P, 500);
 
   //明るさ０で初期化
-  for(int i=0;i<10;i++){
+  for(int i=0;i<RGBLED_NUM;i++){
     V_raw[i]=0;
   }
-  digitalWrite(BLINK_LED, HIGH);//OFF
+  digitalWrite(LED0, HIGH);//OFF
 }
 
-int B_brt;//背面LEDの明るさ変数
 
 void loop() {
   static uint8_t cnt;//0-255
   cnt++;
-
-  B_brt=0;
 
     uint8_t H;//色環 0-255
     uint8_t S;//彩度
@@ -133,11 +133,6 @@ void loop() {
       }
   
       
-
-
-
-
-
       //LEDに設定値を送信
       pixels.setPixelColor(SEL[i], pixels.ColorHSV(H_tmp<<8,S,V_tmp));
       //Hは本来0-65535だけれどそんなに解像度必要ないので0-255で計算。出力するときに8ビットシフトして帳尻を合わせてある
@@ -170,6 +165,9 @@ void loop() {
     }
 
 
+    //設定を反映
+    pixels.show();
+
 
 
     int T_sense0 = ADCTouch.read(KAMI_P,10) -T_offset[KAMI];   //髪の毛タッチ
@@ -181,21 +179,21 @@ void loop() {
     //髪タッチ検知
     if(50 < T_sense0){
         //フラグがまだ立っていなければ以下を実行
-        if(T_flag[KAMI]==0){
-          digitalWrite(BLINK_LED, LOW);//ON
+        if(T_flag[KAMI]==0){          
+          digitalWrite(LED0, LOW);//ON
           if (BRT_LEVEL == 0)     BRT_LEVEL = 1;
           else if(BRT_LEVEL == 1) BRT_LEVEL = 2;
           else if(BRT_LEVEL == 2) BRT_LEVEL = 3;
           else                    BRT_LEVEL = 0;
           //明るさ変更したら即反映
-          for(int i=0;i<8;i++){
+          for(int i=0;i<RGBLED_NUM;i++){
             V_raw[i]=BRT_VAL[BRT_LEVEL];
           }
           //了解コール
           pixels.clear();
           pixels.show();
           delay(8);
-          digitalWrite(BLINK_LED, HIGH);//OFF         
+          digitalWrite(LED0, HIGH);//OFF         
         }
         T_flag[KAMI]=1;//フラグを立てることで立ち上がり時のみ実行
     }else{
@@ -205,7 +203,7 @@ void loop() {
 
     //胸タッチ検出
     if(50 < T_sense1){
-      digitalWrite(BLINK_LED, LOW);//ON
+      digitalWrite(LED0, LOW);//ON
       if(254<FuryGuage){//怒りがマックスになったら脈動させる
         delay(200);
         FuryGuage=200;
@@ -214,7 +212,7 @@ void loop() {
       if((FuryGuage)<255) FuryGuage = FuryGuage + 1;
       T_flag[MUNE]=1;
     }else{
-      digitalWrite(BLINK_LED, HIGH);//OFF
+      digitalWrite(LED0, HIGH);//OFF
       if(0<(FuryGuage-12)) FuryGuage = FuryGuage - 12;
       if(0<FuryGuage) FuryGuage = FuryGuage - 1;
       T_flag[MUNE]=0;
@@ -224,7 +222,7 @@ void loop() {
     if(50 < T_sense2){
         //フラグがまだ立っていなければ以下を実行
         if(T_flag[SKIRT]==0){          
-          digitalWrite(BLINK_LED, LOW);//ON
+          digitalWrite(LED0, LOW);//ON
           if (PATTERN_MODE == 0)     PATTERN_MODE = 1;
           else if(PATTERN_MODE == 1) PATTERN_MODE = 2;
           else                       PATTERN_MODE = 0;
@@ -233,7 +231,7 @@ void loop() {
           pixels.clear();
           pixels.show();
           delay(20);
-          digitalWrite(BLINK_LED, HIGH);//OFF         
+          digitalWrite(LED0, HIGH);//OFF         
         }
         T_flag[SKIRT]=1;//フラグを立てることで立ち上がり時のみ実行
     }else{
@@ -247,8 +245,6 @@ void loop() {
 //  Serial.println(T_sense1);
 //  Serial.println(T_sense2);
 
-    //設定を反映
-    pixels.show();
 
     
     delay(15);
